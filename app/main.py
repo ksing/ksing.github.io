598dca86d318ebe54014d6f529e711449d67995e
Kush Singhal <singhalkushagra@gmail.com>
2025-11-03 00:00:39 +0100

    Add air app

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi.applications import FastAPI
+import air
+
+app = air.Air()
+jinja = air.JinjaRenderer(directory="templates")
+api = FastAPI()
+
+
+@app.get("/")
+async def index(request: air.Request) -> None:
+    return jinja(request, "index.html")
+
+
+@api.get("/")
+def api_root():
+    return {"message": "Awesome SaaS is powered by FastAPI"}
+
+
+app.mount("/api", api)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
